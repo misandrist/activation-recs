6024632627e07894b53c7c1f777dcf09fb311c61
Evan Cofsky <evan@theunixman.com>
2017-02-26 13:43:23 -0600

    Use right C++ std, add LadyClaire's code.

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 CXX = clang++-3.8
-CXXFLAGS = -Weverything -std=c++11 -Wno-c++98-compat -g3
+CXXFLAGS = -Weverything -std=c++14 -Wno-c++98-compat -g3
 
-NAMES = factoradic fib
+SRCS = $(wildcard *.cc)
+NAMES = $(SRCS:%.cc=%)
 
-.PHONY: all
+.PHONY: all clean run
 
 all: $(NAMES)
+run: $(NAMES)
+	for n in $^; do ./$$n; done
 
-fib: CXXFLAGS += -lgmp -lgmpxx
 clean:
 	rm -f $(NAMES)
+
+flat-fib: CXXFLAGS += -lgmp -lgmpxx
---
+++ lady-claire-01.cc
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <memory>
+#include <cstdint>
+
+typedef uint64_t fact_t;
+typedef uint64_t fp_t;
+
+fact_t factorial(fact_t n);
+fact_t factoral_flat(fact_t n);
+
+struct fact_frame {
+    fact_t n;
+    fp_t loc;
+};
+
+template <typename T>
+class Stack {
+private:
+    std::size_t          top;
+    std::size_t          max;
+    std::unique_ptr<T[]> arr;
+public:
+    Stack<T>(std::size_t size=2048) {
+        top = 0;
+        max = size;
+        arr = std::make_unique<T[]>(size);
+    }
+    bool pop(T& val) {
+        if (top == 0) {
+            return false;
+        }
+        else {
+            val = arr[--top];
+            return true;
+        }
+    }
+    bool push(T& val) {
+            if (top == max) {
+            return false;
+        }
+        else {
+            arr[top++] = val;
+            return true;
+        }
+    }
+    bool empty() {
+        return top == 0;
+    }
+    std::size_t size() {
+        return top;
+    }
+};
+
+fact_t factorial(fact_t n) {
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+fact_t factoral_flat(fact_t n) {
+    Stack<fact_frame> s;
+    fact_frame f; f.n = n; f.loc = 0; s.push(f);
+    fact_t ret = 1;
+    while (!s.empty()) {
+        s.pop(f);
+        switch(f.loc) {
+        case 0:
+            if (f.n <= 1)
+                ret = 1;
+            else {
+                f.loc = 1; s.push(f);
+                fact_frame g; g.n = f.n - 1; g.loc = 0;
+                s.push(g);
+            }
+            break;
+        case 1:
+            ret *= f.n;
+            break;
+        }
+    }
+    return ret;
+}
+
+int main() {
+    std::cout << factorial(15) << std:: endl << factoral_flat(15) << std::endl;
+    return 0;
+}
