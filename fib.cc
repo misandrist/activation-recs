65b315634fe9ca52d4d34c7fc77151570daf904f
Evan Cofsky <evan@theunixman.com>
2017-02-25 19:02:51 -0600

    Use a stack.

--- fib.cc
+++ fib.cc
@@ -1,26 +1,42 @@
 #include <cstdio>
 #include <cstdint>
+#include <cassert>
 #include <stack>
 
+using namespace std;
+
 typedef uint64_t fib_t;
+typedef stack<fib_t> fib_s;
+
 fib_t fib(fib_t n);
 
 fib_t fib(fib_t n) {
-    fib_t a = 0;
-    fib_t p = 1;
-    fib_t s = 1;
+    fib_s fs;
+
+    if (n == 0)
+        return 0;
+
+    fs.push(0);
+    fs.push(1);
+
     while (n > 1) {
         n -= 1;
-        a = p + s;
-        p = s;
-        s = a;
+
+        assert (fs.size() == 2);
+        fib_t l = fs.top();
+        fs.pop();
+        fib_t k= fs.top();
+        fs.pop();
+
+        fs.push(l);
+        fs.push(k + l);
     }
-    return s;
+    return fs.top();
 }
 
 int main() {
 
-    for (fib_t f=0; f < 20; f++)
+    for (fib_t f=0; f < 40; f++)
         printf("Fib %ld: %ld\n", f, fib(f));
 
     return 0;
